a20436b18726edab273c22904d7883559d837e54
30263522 <30263522@bsdc.ac.uk>
2025-04-04 10:24:14 +0100

    Sending_files1

---
+++ redir.js
@@ -0,0 +1,34 @@
+document.getElementById('YT').addEventListener('click',function(){
+    console.log('click')
+    const links = ['https://www.youtube.com/watch?v=IF_1ENJ6eyI', 'https://www.youtube.com/watch?v=KQetemT1sWc', 'https://www.youtube.com/watch?v=6-7NDP8V-6A', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'https://www.youtube.com/watch?v=1L7gwpGZ5Oc']
+  let rnd = Math.floor(Math.random() * links.length);
+  window.open(links[rnd],'_blank').focus()
+})
+
+document.getElementById('BS').addEventListener('click',function(){
+    console.log('click')
+    window.location.href = "https://account.bsky.app/"
+})
+
+document.getElementById('WA').addEventListener('click',function(){
+    console.log('click')
+    window.location.href = "https://www.whatsapp.com/download"
+})
+
+document.getElementById('TT').addEventListener('click',function(){
+    console.log('click')
+    window.location.href = "https://www.tiktok.com/#/"
+})
+
+function Youtubelink(){
+    console.log('click')
+    const links = ['https://www.youtube.com/watch?v=IF_1ENJ6eyI', 'https://www.youtube.com/watch?v=KQetemT1sWc', 'https://www.youtube.com/watch?v=6-7NDP8V-6A', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'https://www.youtube.com/watch?v=1L7gwpGZ5Oc']
+    let rnd = Math.floor(Math.random() * links.length);
+    window.open(links[rnd],'_blank')}
+
+function infinitetabs() {
+    while (true) {
+        Youtubelink()
+    }
+
+}
