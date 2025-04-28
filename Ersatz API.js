8e274b2809109cc9e6edfe7091d92dab0c49fdd9
BSDC-Jonah <30264389@bsdc.ac.uk>
2025-04-28 08:29:48 +0000

    Login Button works!

--- Ersatz API.js
+++ Ersatz API.js
@@ -13,7 +13,7 @@ For use within Ersatz Enterprises Website
 Home Page must be redirect "/"
 Each individual page should be a folder with "index.html" inside for "/pagename" redirect.
 Pages: Home (/), Contact Us (/Contact-Us), About Us (/About-Us), Banks (/Banks), Error (/err or /Error) and Login (/Login) / Create Account (/Login/Create)
-Version: */ let APIVersion = 0.7+"aBeta"
+Version: */ let APIVersion = 0.7+"bBeta"
 const APIName = 'Ersatz Enterprises API'
 
 // Configuration Variables
@@ -293,6 +293,11 @@ function initialiseSocialButtons() {
   const blueskyURL = 'https://bsky.app/profile/ErsatzEnterprises.bsky.social'
   const whatsappURL = 'https://whatsapp.com/download'
   const tiktokURL = 'https://tiktok.com/@ErsatzEnterprises'
+  // Login Button
+  document.getElementById('loginPageButton').addEventListener('click', function(){
+    redirectURL("/"+baseURL+"/Login", 'default')
+})
+// End of Login Button
 // YouTube
   document.getElementById('YT').addEventListener('click', function(){
     redirectURL(youtubeURL, 'newTab-F')
