2828db33c655edea20934d2194787041a9cb6303
bsdc-KS <166498227+30263522@users.noreply.github.com>
2025-05-02 13:16:37 +0000

    Meow meow

--- Ersatz API.js
+++ Ersatz API.js
@@ -394,6 +394,25 @@ function loginButton() {
 }
 // End of Button Handlers
 
+
+// The Mailto function
+function mailMe(){
+  let emailName = document.getElementById("myMail").value
+  console.log(emailName)
+  let emailText = document.getElementById("myMessage").value
+  console.log(emailText)
+  emailText = emailText.replaceAll(" ",'%20')
+  console.log(emailName)
+  console.log(emailText)
+  const mailtoHandle = document.createElement('a')
+  mailtoHandle.id = "mailto-Click"
+  mailtoHandle.href = `mailto:Support@ersatz.com?subject="Ersatz Contact"&body=${emailText}`
+  document.body.appendChild(mailtoHandle)
+  mailtoHandle.click()
+
+}
+
+
 // Console - Creditor
 console.warn(`
 ${APIName} (Version: ${APIVersion}) successfully started running in the background;
