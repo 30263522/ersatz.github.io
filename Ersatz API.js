d73cc404175f057d0839ba2eb055e3f41b9da2bb
BSDC-Jonah <30264389@bsdc.ac.uk>
2025-05-09 09:54:24 +0000

    Version update

--- Ersatz API.js
+++ Ersatz API.js
@@ -13,7 +13,7 @@ For use within Ersatz Enterprises Website
 Home Page must be redirect "/"
 Each individual page should be a folder with "index.html" inside for "/pagename" redirect.
 Pages: Home (/), Contact Us (/Contact-Us), About Us (/About-Us), Banks (/Banks), Error (/err or /Error) and Login (/Login) / Create Account (/Login/Create)
-Version: */ let APIVersion = 0.8+"5Beta"
+Version: */ let APIVersion = 0.9+"Beta"
 const APIName = 'Ersatz Enterprises API'
 
 // Configuration Variables
